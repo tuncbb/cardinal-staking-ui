e46d4fd1a62f768530525deecc3b36243c96b99d
tuncbb <mrkite10@gmail.com>
2022-08-04 00:45:44 +0300

    change image

--- api/mapping.ts
+++ api/mapping.ts
@@ -75,7 +75,7 @@ export const defaultSecondaryColor = 'rgba(29, 78, 216, 255)'
 export const stakePoolMetadatas: StakePoolMetadata[] = [
   {
     name: 'thornode-elite',
-    displayName: 'ThorNode RPCs',
+    displayName: 'Elite Pass',
     stakePoolAddress: new PublicKey(
       'J79xX6CYxXZzXQSy6g6hkBnnfFs7pRDjqy4nFKjnXvu9'
     ),
@@ -83,7 +83,7 @@ export const stakePoolMetadatas: StakePoolMetadata[] = [
     receiptType: ReceiptType.Original,
     maxStaked: 33, // update with collection size
     imageUrl:
-      'https://creator-hub-prod.s3.us-east-2.amazonaws.com/thornode_pfp_1656943062647.gif',
+      'https://v4pnvyuwfsaiymwlmqr4mvemptw2lmqyrwmilxg2prlw7qrqf3qq.arweave.net/rx7a4pYsgIwyy2QjxlSMfO2lshiNmIXc2nxXb8IwLuE?ext=gif',
     tokenStandard: TokenStandard.NonFungible,
     hideAllowedTokens: false,
     hidden: true,
