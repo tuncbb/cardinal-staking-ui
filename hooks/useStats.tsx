018c184338d69850fb55789696f2828ea605143b
tuncbb <mrkite10@gmail.com>
2022-08-04 14:38:38 +0300

    remove cardinal stats fix

--- hooks/useStats.tsx
+++ hooks/useStats.tsx
@@ -7,43 +7,43 @@ import { useEnvironmentCtx } from 'providers/EnvironmentProvider'
 import { useQuery } from 'react-query'
 import { useStakePoolId } from './useStakePoolId'
 
-export const useStats = () => {
-  const { secondaryConnection } = useEnvironmentCtx()
-  const stakePoolId = useStakePoolId()
-  return useQuery<
-    | {
-        [name: string]: { data: AccountData<StatsEntryData> }
-      }
-    | undefined
-  >(
-    ['useStats', stakePoolId?.toString()],
-    async () => {
-      const statsNames = [
-        'total-active-staked-tokens',
-        'total-active-stake-entries',
-        'total-stake-pools',
-      ]
-      const statsData = await Promise.all(
-        statsNames.map((name) => getStatsEntry(secondaryConnection, name))
-      )
-      return statsData.reduce(
-        (acc, stat) => {
-          const displayName = statsNameMapping.find(
-            (mapp) => mapp.key === stat.parsed.name
-          )!.displayName
-          acc[displayName] = { data: stat }
-          return acc
-        },
-        {} as {
-          [name: string]: { data: AccountData<StatsEntryData> }
-        }
-      )
-    },
-    {
-      refetchInterval: 3000,
-    }
-  )
-}
+// export const useStats = () => {
+//   const { secondaryConnection } = useEnvironmentCtx()
+//   const stakePoolId = useStakePoolId()
+//   return useQuery<
+//     | {
+//         [name: string]: { data: AccountData<StatsEntryData> }
+//       }
+//     | undefined
+//   >(
+//     ['useStats', stakePoolId?.toString()],
+//     async () => {
+//       const statsNames = [
+//         'total-active-staked-tokens',
+//         'total-active-stake-entries',
+//         'total-stake-pools',
+//       ]
+//       const statsData = await Promise.all(
+//         statsNames.map((name) => getStatsEntry(secondaryConnection, name))
+//       )
+//       return statsData.reduce(
+//         (acc, stat) => {
+//           const displayName = statsNameMapping.find(
+//             (mapp) => mapp.key === stat.parsed.name
+//           )!.displayName
+//           acc[displayName] = { data: stat }
+//           return acc
+//         },
+//         {} as {
+//           [name: string]: { data: AccountData<StatsEntryData> }
+//         }
+//       )
+//     },
+//     {
+//       refetchInterval: 3000,
+//     }
+//   )
+// }
 
 // const statsNameMapping = [
 //   {
